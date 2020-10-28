657f9a83b2bfb521bf013db86b57ed18f288a74e
PierreSavatte <pierre-savatte@laposte.net>
2020-10-28 18:27:47 +0100

    Add recipe and base_preparation_time to CommandTypes

--- dnk/models/command.py
+++ dnk/models/command.py
@@ -1,15 +1,48 @@
 from enum import Enum
 
+from dnk.settings import PREPARATION_TIME_UNIT
 from dnk.models import RandomlyInitialisable
 
 
-class CommandType(Enum):
-    KEBAB = "kebab"
-    FRENCH_FRIES = "french fries"
+class Ingredients(Enum):
+    BREAD = "bread"
+    LAMB = "lamb"
+    SALAD = "salad"
+    TOMATO = "tomato"
+    ONION = "onion"
+    POTATO = "potato"
+
+
+class CommandTypes(Enum):
+    KEBAB = {
+        "name": "kebab",
+        "base_preparation_time": 4 * PREPARATION_TIME_UNIT,
+        "recipe": {
+            Ingredients.BREAD: 1,
+            Ingredients.LAMB: 1,
+            Ingredients.SALAD: 1,
+            Ingredients.TOMATO: 1,
+            Ingredients.ONION: 1,
+        },
+    }
+    FRENCH_FRIES = {
+        "name": "french fries",
+        "base_preparation_time": 1 * PREPARATION_TIME_UNIT,
+        "recipe": {
+            Ingredients.BREAD: 1,
+            Ingredients.LAMB: 1,
+            Ingredients.SALAD: 1,
+            Ingredients.TOMATO: 1,
+            Ingredients.ONION: 1,
+        },
+    }
 
 
 class Command(RandomlyInitialisable):
-    expected_enums_at_init = [CommandType]
+    expected_enums_at_init = [CommandTypes]
 
     def __init__(self, command_type):
-        self.name = command_type.value
+        values = command_type.value
+        self.name = values["name"]
+        self.base_preparation_time = values["base_preparation_time"]
+        self.recipe = values["recipe"]
--- tests/models/test_command.py
+++ tests/models/test_command.py
@@ -1,10 +1,26 @@
-from dnk.models.command import Command, CommandType
+import pytest
+
+from dnk.models.command import Command, CommandTypes
 
 
-def test_command_has_name():
-    command = Command(command_type=CommandType.KEBAB)
+@pytest.fixture(
+    params=list(CommandTypes),
+    ids=[
+        f"CommandType={command_type.name}"
+        for command_type in list(CommandTypes)
+    ],
+)
+def command_type(request):
+    return request.param
 
-    assert command.name == "kebab"
+
+def test_command_has_all_data_needed(command_type):
+    command = Command(command_type=command_type)
+    values = command_type.value
+
+    assert command.name == values["name"]
+    assert command.base_preparation_time == values["base_preparation_time"]
+    assert command.recipe == values["recipe"]
 
 
 def test_get_random_command():
